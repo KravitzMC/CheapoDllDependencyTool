f3208556a74eb0c42b511399f4eec185a22b6755
marshal <marshalcraft@gmail.com>
2016-06-07 07:11:03 -0400

    Initial Commit

---
+++ CheapoDllDependencyTool/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace CheapoDllDependencyTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 60);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(830, 263);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(854, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click_1);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 350);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ CheapoDllDependencyTool/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheapoDllDependencyTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Cheapo Dll Dependency Tool";
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void openToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            byte[] ExeByteArray;
+            string[] DllFileNames;
+            int ExeByteArrayPossibleIndexSize = 0;
+            int[] ExeByteArrayPossibleIndex;
+            int ExeDllFileNamesArrayIndexSize = 0;
+
+
+            DialogResult userClickedOK = openFileDialog1.ShowDialog();
+
+            // Process input if the user clicked OK.
+            if (userClickedOK == DialogResult.OK)
+            {
+                ExeByteArray = System.IO.File.ReadAllBytes(openFileDialog1.FileName);
+                for (int i = 0; i < ExeByteArray.Length; i++)
+                {
+                    if (ExeByteArray[i] == '.')
+                    {
+                        if (i <= ExeByteArray.Length - 4)
+                        {
+                            if (ExeByteArray[i + 1] == 'd' &&
+                               ExeByteArray[i + 2] == 'l' &&
+                               ExeByteArray[i + 3] == 'l')
+                            {
+                                ExeByteArrayPossibleIndexSize++;
+                            }
+                        }
+                    }
+                }
+                DllFileNames = new string[ExeByteArrayPossibleIndexSize];
+                ExeByteArrayPossibleIndex = new int[ExeByteArrayPossibleIndexSize];
+                ExeByteArrayPossibleIndexSize = 0;
+                for (int i = 0; i < ExeByteArray.Length; i++)
+                {
+                    if (ExeByteArray[i] == '.')
+                    {
+                        if (i <= ExeByteArray.Length - 4)
+                        {
+                            if (ExeByteArray[i + 1] == 'd' &&
+                               ExeByteArray[i + 2] == 'l' &&
+                               ExeByteArray[i + 3] == 'l')
+                            {
+                                ExeByteArrayPossibleIndex[ExeByteArrayPossibleIndexSize] = i;
+                                ExeByteArrayPossibleIndexSize++;
+                            }
+                        }
+                    }
+                }
+                bool WhiteSpaceFoundOrTooLongName = false;
+                int WhiteSpaceFinderCount = 0;
+
+                for (int i = 0; i < ExeByteArrayPossibleIndexSize; i++)
+                {
+                    WhiteSpaceFoundOrTooLongName = false;
+                    WhiteSpaceFinderCount = 0;
+                    byte[] filenamebytes;
+
+                    do
+                    {
+                        if (ExeByteArray[ExeByteArrayPossibleIndex[i] - WhiteSpaceFinderCount] == ' ' ||
+                           WhiteSpaceFinderCount > 50)
+                        {
+                            WhiteSpaceFoundOrTooLongName = true;
+                        }
+                        WhiteSpaceFinderCount++;
+                    }
+                    while (WhiteSpaceFoundOrTooLongName == false);
+
+                    filenamebytes = new byte[WhiteSpaceFinderCount];
+                    WhiteSpaceFoundOrTooLongName = false;
+                    WhiteSpaceFinderCount = 0;
+
+                    do
+                    {
+                        if (ExeByteArray[ExeByteArrayPossibleIndex[i] - WhiteSpaceFinderCount] == ' ' ||
+                           WhiteSpaceFinderCount > 50)
+                        {
+                            WhiteSpaceFoundOrTooLongName = true;
+                        }
+                        else
+                        {
+                            filenamebytes[WhiteSpaceFinderCount] = ExeByteArray[ExeByteArrayPossibleIndex[i] - WhiteSpaceFinderCount];
+                        }
+
+                        WhiteSpaceFinderCount++;
+                    }
+                    while (WhiteSpaceFoundOrTooLongName == false);
+                 
+                    DllFileNames[i] = System.Text.Encoding.ASCII.GetString(filenamebytes);
+                }
+                for (int i = 0; i < DllFileNames.Length; i++)
+                {
+                    listView1.Items.Add(DllFileNames[i]);
+                }
+                listView1.View = View.Tile;// LargeIcon;
+            }
+        }
+    }
+}
